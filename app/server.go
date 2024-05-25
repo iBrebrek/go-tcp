52729830d2883cef85f98f0d1e8520f3abcad9b0
Ivica <i.brebrek@gmail.com>
2024-05-25 20:32:43 +0200

    Refactor

--- app/server.go
+++ app/server.go
@@ -33,13 +33,29 @@ func handleConnection(conn net.Conn) {
 	buffer := make([]byte, 1024)
 	conn.Read(buffer)
 	request := string(buffer)
+	resp := dispatch(parseRequest(request))
+	conn.Write([]byte(resp))
+}
+
+func parseRequest(request string) (method, path, body string) {
 	fmt.Println("~~~ request ~~~\n", request)
 	requestParts := strings.Split(request, "\r\n")
 	requestLine := requestParts[0]
-	path := strings.Split(requestLine, " ")[1]
+	lineParts := strings.Split(requestLine, " ")
+	method = lineParts[0]
+	path = lineParts[1]
+	body = requestParts[2]
+	return // named result parameters
+}
+
+func dispatch(method string, path string, body string) string {
+	if method != "GET" {
+		return "HTTP/1.1 405 Method Not Allowed\r\n\r\n"
+	}
+
 	if path == "/" || path == "/index.html" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nyes"))
+		return "HTTP/1.1 200 OK\r\n\r\n"
 	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\nno"))
+		return "HTTP/1.1 404 Not Found\r\n\r\n"
 	}
 }
